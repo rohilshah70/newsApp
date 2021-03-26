806c7118f87856883bbc4ca5d2b0fa8b9d883d1f
Rohil <rohilshah70@gmail.com>
2021-03-26 17:48:06 +0530

    Added fragment transition to news list

--- app/src/main/java/com/example/newsapp/activity/MotionFragment.java
+++ app/src/main/java/com/example/newsapp/activity/MotionFragment.java
@@ -1,25 +1,43 @@
 package com.example.newsapp.activity;
 
+import android.animation.LayoutTransition;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.newsapp.R;
 
 public class MotionFragment extends Fragment {
 
-
+    private String TAG = MotionFragment.class.getName();
+    private TextView mTextView;
+    private View mRootView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.motion_layout, container, false);
+        mRootView = inflater.inflate(R.layout.motion_layout, container, false);
+        mTextView = mRootView.findViewById(R.id.open_news_list);
 
-        return rootView;
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view)
+                        .navigate(R.id.action_motionFragment_to_articleListFragment);
+
+            }
+        });
+        Log.d( TAG, "In onCreateView");
+
+        return mRootView;
     }
 }

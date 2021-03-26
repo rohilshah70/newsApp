1ed8151629b2f15e6600e920891930035dea7a40
Rohil <rohilshah70@gmail.com>
2021-03-26 16:11:25 +0530

    Added 1st animation

---
+++ app/src/main/java/com/example/newsapp/activity/MotionFragment.java
@@ -0,0 +1,25 @@
+package com.example.newsapp.activity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.newsapp.R;
+
+public class MotionFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.motion_layout, container, false);
+
+        return rootView;
+    }
+}

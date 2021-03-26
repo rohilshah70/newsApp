1c04323ebdba6e0ce2d70e07995f0b7dc7c82c22
Rohil <rohilshah70@gmail.com>
2021-03-27 04:45:06 +0530

    Handle navigation for cross button

--- app/src/main/java/com/example/newsapp/activity/ArticleDetailFragment.java
+++ app/src/main/java/com/example/newsapp/activity/ArticleDetailFragment.java
@@ -7,10 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.newsapp.R;
 import com.example.newsapp.model.Article;
@@ -18,18 +21,31 @@ import com.example.newsapp.model.Article;
 public class ArticleDetailFragment extends Fragment {
 
     private Article mArticle;
+    private TextView mHeaderText;
+    private ImageView mCrossImage;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.article_fragment, container, false);
+        mHeaderText = rootView.findViewById(R.id.headerText1);
+        mCrossImage = rootView.findViewById(R.id.crossImage1);
         WebView myWebView = rootView.findViewById(R.id.webview);
         myWebView.clearCache(true);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
         webSettings.setJavaScriptEnabled(true);
 
+        mCrossImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view)
+                        .navigate(R.id.action_articleDetailFragment_to_articleListFragment);
+            }
+        });
+
         mArticle = (Article) getArguments().getSerializable("article");
+        mHeaderText.setText(mArticle.getTitle());
         myWebView.loadUrl(mArticle.getUrl());
 
         return rootView;
--- app/src/main/java/com/example/newsapp/activity/ArticleListFragment.java
+++ app/src/main/java/com/example/newsapp/activity/ArticleListFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -24,12 +26,14 @@ import java.util.List;
 public class ArticleListFragment extends Fragment {
 
     private NewsViewModel mNewsViewModel;
+    private ImageView mCrossImage;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.article_list_fragment, container, false);
+        mCrossImage = rootView.findViewById(R.id.crossImage);
         RecyclerView recyclerView = rootView.findViewById(R.id.main_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
@@ -46,6 +50,14 @@ public class ArticleListFragment extends Fragment {
                 else {
                     makeToast("No internet connection or no cached data");
                 }
+            }
+        });
+
+        mCrossImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view)
+                        .navigate(R.id.action_articleListFragment_to_motionFragment);
             }
         });
         return rootView;
